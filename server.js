8bfb8357ade4467458025b8c692f739eaf83c197
Eoin Smith <eoinsmith001@gmail.com>
2014-11-01 16:39:32 +1000

    added model and local mongo config to server.js

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ var bodyParser = require( 'body-parser' );
 var methodOverride = require( 'method-override' );
 
 
-// config
+// config ---------------------------------------------------
 // mongoDB db on modulus.io
-mongoose.connect( 'mongodb://node:node@mongo.onmodulus.net:27017/uw03mypu' );
+// mongoose.connect( 'mongodb://node:node@mongo.onmodulus.net:27017/uw03mypu' );
+// from http://mongoosejs.com/docs/index.html examples
+mongoose.connect( 'mongodb://localhost:27017/test' );
 
 // set static files location - /public/img will be /img for users
 app.use( express.static( __dirname + '/public' ) );
@@ -22,7 +24,19 @@ app.use( bodyParser.json() );
 app.use( bodyParser.json( { type: 'application/vnd.api+json' } ) );
 app.use( methodOverride );
 
+// model  ---------------------------------------------------
 
-// listen
-app.listen( 8080 );
-console.log( 'app listening on port 8080' );
+var Todo = mongoose.model( 'Todo', {
+  text: String
+});
+
+// routes ---------------------------------------------------
+
+app.get( '/api/todos', function( req, res ) {
+  
+});
+
+// listen ---------------------------------------------------
+var port = 8081;
+app.listen( port );
+console.log( 'app listening on port %d', port );
