ebc0f12b44fd219cdccc3dc6a72b13e2fa2533d3
Eoin Smith <eoinsmith001@gmail.com>
2014-11-02 14:03:48 +1000

    added routing for get, post and delete

--- server.js
+++ server.js
@@ -33,7 +33,44 @@ var Todo = mongoose.model( 'Todo', {
 // routes ---------------------------------------------------
 
 app.get( '/api/todos', function( req, res ) {
-  
+  Todo.find( function(err,todos) {
+    if (err) 
+      res.send(err)
+    res.json(todos);  // return todos in json format
+  }); 
+});
+
+app.post( '/api/todos', function( req, res ) {
+  // Create a todo, info coming from Angular AJAX
+  Todo.create({
+    text: req.body.text,
+    done: false
+  }, function(err,todo) {
+    if (err)
+      res.send(err);
+    // find and return all todos after creating a new one
+    Todo.find(function(err,todos) {
+      if (err)
+        res.send(err)
+      res.json(todos);
+    });
+  });
+});
+
+// delete
+app.delete( '/api/todos/:todo_id', function( req, res ) {
+  Todo.remove({
+    _id: req.params.todo_id
+  }, function(err,todo) {
+    if (err)
+      res.send(err);
+    // again return all todos
+    Todo.find(function(err,todos) {
+      if (err)
+        res.send(err);
+      res.json(todos);
+    });
+  });
 });
 
 // listen ---------------------------------------------------
